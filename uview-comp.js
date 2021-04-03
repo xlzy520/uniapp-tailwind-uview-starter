bae44ea98a1c9c0fecc9ebe0515bf4677e23ab90
xlzy <1017866168@qq.com>
2021-04-03 14:17:12 +0800

    为了uview组件库代码提示，不需要使用

---
+++ uview-comp.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+
+// uview组件，用于webstorm代码提示
+import UCalendar from 'uview-ui/components/u-calendar/u-calendar'
+import UButton from 'uview-ui/components/u-button/u-button'
+import URadio from 'uview-ui/components/u-radio/u-radio'
+import URadioGroup from 'uview-ui/components/u-radio-group/u-radio-group'
+import UForm from 'uview-ui/components/u-form/u-form'
+import UFormItem from 'uview-ui/components/u-form-item/u-form-item'
+import UInput from 'uview-ui/components/u-input/u-input'
+import UCol from 'uview-ui/components/u-col/u-col'
+import UAvatar from 'uview-ui/components/u-avatar/u-avatar'
+import USwiper from 'uview-ui/components/u-swiper/u-swiper'
+import UTag from 'uview-ui/components/u-tag/u-tag'
+import UCellItem from 'uview-ui/components/u-cell-item/u-cell-item'
+import UCellGroup from 'uview-ui/components/u-cell-group/u-cell-group'
+import URow from 'uview-ui/components/u-row/u-row'
+import UUpload from 'uview-ui/components/u-upload/u-upload'
+import UPicker from 'uview-ui/components/u-picker/u-picker'
+import USelect from 'uview-ui/components/u-select/u-select'
+import UIcon from 'uview-ui/components/u-icon/u-icon'
+import UTabs from 'uview-ui/components/u-tabs/u-tabs'
+import UImage from 'uview-ui/components/u-image/u-image'
+import UDivider from 'uview-ui/components/u-divider/u-divider'
+import UNoticeBar from 'uview-ui/components/u-notice-bar/u-notice-bar'
+import UMask from 'uview-ui/components/u-mask/u-mask'
+// import UTable from "uview-ui/components/u-table/u-table";
+import UTimeLine from 'uview-ui/components/u-time-line/u-time-line'
+import UTimeLineItem from 'uview-ui/components/u-time-line-item/u-time-line-item'
+import UField from 'uview-ui/components/u-field/u-field'
+import UEmpty from 'uview-ui/components/u-empty/u-empty'
+
+Vue.component('u-upload', UUpload)
+Vue.component('u-row', URow)
+Vue.component('u-tag', UTag)
+Vue.component('u-calendar', UCalendar)
+Vue.component('u-button', UButton)
+Vue.component('u-radio-group', URadioGroup)
+Vue.component('u-radio', URadio)
+Vue.component('u-form', UForm)
+Vue.component('u-form', UForm)
+Vue.component('u-form-item', UFormItem)
+Vue.component('u-input', UInput)
+Vue.component('u-col', UCol)
+Vue.component('u-avatar', UAvatar)
+Vue.component('u-swiper', USwiper)
+Vue.component('u-cell-item', UCellItem)
+Vue.component('u-cell-group', UCellGroup)
+Vue.component('u-picker', UPicker)
+Vue.component('u-select', USelect)
+Vue.component('u-icon', UIcon)
+Vue.component('u-tabs', UTabs)
+Vue.component('u-image', UImage)
+Vue.component('u-divider', UDivider)
+Vue.component('u-notice-bar', UNoticeBar)
+Vue.component('u-mask', UMask)
+// Vue.component('u-table', UTable)
+Vue.component('u-time-line', UTimeLine)
+Vue.component('u-time-line-item', UTimeLineItem)
+Vue.component('u-field', UField)
+Vue.component('u-empty', UEmpty)
+
