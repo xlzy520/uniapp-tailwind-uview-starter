bb3449af18de16d147e3aaba4636d347c87e62bf
xlzy <1017866168@qq.com>
2021-04-03 19:04:30 +0800

    增加首页列表展示案例，接口请求格式

---
+++ src/components/TabWithDataList/index.vue
@@ -0,0 +1,94 @@
+<template>
+<view class="">
+  <view class="tab-list-header" v-if="showTab">
+    <u-tabs :list="tabs" :is-scroll="false" active-color="#6A88F3" inactive-color="#999999"
+            :current="current" @change="tabChange"></u-tabs>
+  </view>
+  <divider v-show="showTab" />
+  <view class="tab-list-content">
+    <view class="flex justify-between items-center list-item" v-for="item in listData"
+          :key="item.title" @click="handleItemClick(item)">
+      <u-image :src="item.imgUrl" width="192" height="192" class="cover"></u-image>
+      <view class="content">
+        <view class="title list-item-title">{{ item.title }}</view>
+        <view class="list-item-subtitle">{{ item.desc }}</view>
+      </view>
+    </view>
+
+  </view>
+  <u-empty text="暂无数据" mode="list" v-if="!listData.length"></u-empty>
+</view>
+</template>
+
+<script>
+
+export default {
+  name: 'TabWithDataList',
+  props: {
+    showTab: {
+      type: Boolean,
+      default: true,
+    },
+    noStar: {
+      type: Boolean,
+      default: true,
+    },
+    listType: {
+      type: String,
+      default: '',
+    },
+    columnType: {
+      type: String,
+      default: '',
+    },
+    tabs: {
+      type: Array,
+      default: () => ([]),
+    },
+    listData: {
+      type: Array,
+      default: () => ([]),
+    },
+
+  },
+  data() {
+    return {
+      current: 0,
+    }
+  },
+  onLoad(options) {
+
+  },
+  methods: {
+    tabChange(index, item) {
+      this.current = index
+      this.$emit('tabChange', index, item)
+    },
+    handleItemClick(item) {
+      this.$emit('itemClick', item)
+    },
+
+  },
+}
+</script>
+<style lang="scss">
+  .tab-list-content{
+    padding: 0 30upx;
+    min-height: 400upx;
+  }
+  .list-item {
+    padding: 30rpx 0;
+    border-bottom: 2rpx solid #E4E4E4;
+    margin-bottom: 22rpx;
+    .cover {
+      border-radius: 14upx;
+    }
+    .content {
+      margin-left: 30upx;
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+    }
+  }
+
+</style>
---
+++ src/components/divider/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="divider-box" :style="{height: height, width: width, backgroundColor: bgColor}"></view>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    };
+  },
+  props: {
+    bgColor: {
+      type: String,
+      default: '#FBFBFB'
+    },
+    width: {
+      type: String,
+      default: '750rpx'
+    },
+    height: {
+      type: String,
+      default: '20rpx'
+    },
+
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+  .divider-box{
+    margin-left: -30rpx;
+  }
+
+</style>
