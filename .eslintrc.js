45271e9ab69e89657f74da759294776a140dc2e8
xlzy <1017866168@qq.com>
2021-04-03 14:48:32 +0800

    增加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,106 @@
+// .eslintrc.js 文件内容， 此处是eslint的规则定义， 可自行修改，可参考eslint官网修改
+module.exports = {
+  root: true,
+  extends: [
+    // 'eslint:recommended',
+    'plugin:vue/essential',
+    'airbnb-base'
+  ],
+  parserOptions: {
+    ecmaVersion: 9,
+    ecmaFeatures: {
+      jsx: false,
+    },
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  plugins: [
+    'node',
+    'vue'
+  ],
+  rules: {
+    'arrow-parens': 'off',
+    'no-tabs': 'off',
+    complexity: ['error', 10],
+    'global-require': 'off',
+    'handle-callback-err': [
+      'error',
+      '^(err|error)$'
+    ],
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'linebreak-style': 'off',
+    'no-catch-shadow': 'error',
+    'no-continue': 'off',
+    'no-div-regex': 'warn',
+    'no-else-return': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'no-shadow': 'off',
+    // enable console for this project
+    'no-console': 'off',
+    'no-multi-assign': 'off',
+    'no-underscore-dangle': 'off',
+    'node/no-deprecated-api': 'error',
+    'node/process-exit-as-throw': 'error',
+    'operator-linebreak': [
+      'error',
+      'after',
+      {
+        overrides: {
+          ':': 'before',
+          '?': 'before',
+        },
+      }
+    ],
+    'prefer-arrow-callback': 'off',
+    'prefer-destructuring': 'off',
+    'prefer-template': 'off',
+    'quote-props': [
+      1,
+      'as-needed',
+      {
+        unnecessary: true,
+      }
+    ],
+    semi: [
+      'error',
+      'never'
+    ],
+    camelcase: [
+      'error',
+      {
+        properties: 'never',
+      }
+    ],
+    'func-names': ['error', 'as-needed'],
+    'comma-dangle': [
+      'error',
+      {
+        functions: 'never',
+        arrays: 'never',
+        imports: 'never',
+        exports: 'never',
+        objects: 'always-multiline',
+      }
+    ],
+  },
+  globals: {
+    window: true,
+    document: true,
+    App: true,
+    Page: true,
+    Component: true,
+    Behavior: true,
+    wx: true,
+    worker: true,
+    getApp: true,
+    uni: true,
+  },
+}
