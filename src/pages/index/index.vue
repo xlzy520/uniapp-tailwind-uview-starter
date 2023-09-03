f8d642e9b66d06fa1eebf9cde4d0947543457007
xlzy <1017866168@qq.com>
2023-09-03 10:42:47 +0800

    增加自动补充置顶评论功能

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -249,6 +249,7 @@ import updateDialog from './update-dialog.vue';
 import ImportAndExport from './importAndExport.vue';
 import setCkDialog from './set-ck-dialog.vue';
 import setTopReplyDialog from './set-top-reply-dialog.vue';
+import { getReplyData } from '@/utils/reply';
 
 const innerAudioContext = uni.createInnerAudioContext();
 export default {
@@ -624,41 +625,7 @@ export default {
         this.$alert(message, '提示');
         return;
       }
-      const message = topReply.message;
-      const img1 = topReply.img1;
-      const img2 = topReply.img2;
-      const getImgText = (height, width, img, size) => {
-        return `{"img_height":${height},"img_width":${width},"img_src":"${img}","img_size":${size}}`;
-      };
-      const img1Size = await getImgSize(img1);
-      if (!img1Size) {
-        alert('图片1加载失败, 请重新选择文件上传');
-        return;
-      }
-      let pictures = `[${getImgText(
-        img1Size.height,
-        img1Size.width,
-        img1,
-        img1Size.size,
-      )}]`;
-      if (img2) {
-        const img2Size = await getImgSize(img2);
-        if (!img2Size) {
-          alert('图片2加载失败, 请重新填写图片链接');
-          return;
-        }
-        pictures = `[${getImgText(
-          img1Size.height,
-          img1Size.width,
-          img1,
-          img1Size.size,
-        )},${getImgText(
-          img2Size.height,
-          img2Size.width,
-          img2,
-          img2Size.size,
-        )}]`;
-      }
+      const { message, pictures } = await getReplyData(topReply);
       return sendReply({
         oid: video.aid,
         cookie: video.cookie,
--- src/pages/index/set-top-reply-dialog.vue
+++ src/pages/index/set-top-reply-dialog.vue
@@ -6,10 +6,12 @@
     @close="close"
   >
     <div class="text-red-500">
-      <a
+      <el-link
+        type="primary"
         href="https://service-8zqb5ngm-1253419200.gz.apigw.tencentcs.com/bilibili/upload.html"
-        >图片上传点这里</a
       >
+        图片上传点这里
+      </el-link>
     </div>
     <el-form
       ref="form"
@@ -18,10 +20,31 @@
       label-width="200px"
       label-position="top"
     >
-      <el-form-item label="文字" prop="message">
+      <el-form-item label="发置顶评论的CK">
+        <el-radio-group v-model="form.ckType">
+          <el-radio :label="1">当前视频UP主的CK</el-radio>
+          <el-radio :label="2">其他账号的CK</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item
+        v-if="form.ckType === 2"
+        label="发置顶评论的CK"
+        prop="cookie"
+      >
         <el-input
           type="textarea"
-          :autosize="{ minRows: 4, maxRows: 4 }"
+          :autosize="{ minRows: 4, maxRows: 6 }"
+          v-model.trim="form.cookie"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="需要发布的文字，以逗号分隔，发布时会随机从里面取一句话"
+        prop="message"
+      >
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 4, maxRows: 6 }"
           v-model.trim="form.message"
         ></el-input>
       </el-form-item>
@@ -31,6 +54,7 @@
       <el-form-item label="图片链接2">
         <el-input v-model.trim="form.img2"></el-input>
       </el-form-item>
+
       <el-form-item>
         <div class="text-center">
           <el-button type="primary" @click="onSubmit">确定</el-button>
@@ -41,6 +65,8 @@
 </template>
 
 <script>
+import { getSpaceInfo } from '@/api/bilibili';
+
 export default {
   props: {
     video: {
@@ -50,7 +76,9 @@ export default {
   },
   data() {
     return {
-      form: {},
+      form: {
+        ckType: 1,
+      },
       rules: {
         message: [
           {
@@ -66,9 +94,40 @@ export default {
     close() {
       this.$emit('close');
     },
+    getSpaceInfo() {
+      return getSpaceInfo(this.form.cookie)
+        .then((res) => {
+          const ownerMid = this.video.owner.mid;
+          if (ownerMid === res.mid) {
+            this.$message.success('配置的CK是当前视频UP主的CK');
+          }
+          return res;
+        })
+        .catch(() => {
+          this.$message.error('CK不正确或者已过期, 请配置正确的CK');
+        });
+    },
     onSubmit() {
-      this.$refs.form.validate((valid) => {
+      this.$refs.form.validate(async (valid) => {
         if (valid) {
+          if (this.form.ckType === 1) {
+            const videoCookie = this.video.cookie;
+            if (!videoCookie) {
+              this.$message.error('当前视频没有CK，请先配置CK');
+              return;
+            }
+            this.form.cookie = this.video.cookie;
+          } else {
+            this.form.cookie = this.form.cookie.trim();
+          }
+          const userInfo = await this.getSpaceInfo();
+          if (!userInfo) {
+            return;
+          }
+          this.form.message = this.form.message
+            .replace(/\s+/g, '') // 替换所有空格
+            .replace(/\s/g, '') // 替换单个空格
+            .replace(/，/g, ','); // 替换所有逗号为英文逗号
           this.$emit('submit', {
             topReply: this.form,
           });
@@ -79,7 +138,12 @@ export default {
   mounted() {
     const video = { ...this.video };
     if (video) {
-      this.form = video.topReply;
+      const topReply = video.topReply || {};
+      this.form = {
+        ...video.topReply,
+        ckType: topReply.ckType ?? 1,
+        cookie: topReply.ckType === 1 ? video.cookie : topReply.cookie ?? '',
+      };
     }
   },
 };
