9a22293c479bd942503134efcde48d884e7ea497
zhibi <1017866168@qq.com>
2024-08-11 18:39:12 +0800

    增加更新提示

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -729,6 +729,16 @@ export default {
           this.hasUpdate = true;
         }
         this.getVideoStatsList();
+        const source_version = localStorage.getItem('source_version');
+        if (!source_version) {
+          this.$alert('增加服务器端读取数据源的功能，使用服务端的数据源，可以不再需要自己的电脑启动客户端，只需要直接打开网页即可，也不会再出现客户端死机的情况。再页面顶部，可以自由切换数据源！', '重要提示', {
+            type: 'success',
+            confirmButtonText: '确定',
+            callback: () => {
+              localStorage.setItem('source_version', 'true');
+            },
+          });
+        }
       })
       .catch(() => {
         clearInterval(this.deleteReplyInterval);
