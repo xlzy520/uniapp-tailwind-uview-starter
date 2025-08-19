a824190bf5beedb272c4a3b93ae67d10914b4015
zhibi <1017866168@qq.com>
2025-08-19 21:20:04 +0800

    调整域名

--- src/api/bilibili.js
+++ src/api/bilibili.js
@@ -300,7 +300,7 @@ export const checkLicense = (license) => {
   }
   
   return axios
-    .get(origin + `/auth?key=${license}&extId=${extId}&type=视频数据监控`, {
+    .get(origin + `/auth?key=${license}&extId=${extId}&type=自动回复设置`, {
       timeout: 5000
     })
     .then((res) => {
